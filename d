51dc49c898d40570590208e2eea271964ead2026
javierverb <javier2mil8@gmail.com>
2016-10-26 17:15:17 -0300

    Falta scrapear imágenes

---
+++ d
@@ -0,0 +1,142 @@
+{
+  "196472": {
+    "id": "196472",
+    "image": null,
+    "name": "Geko",
+    "rank": "5",
+    "stats": {
+      "agility": "10927",
+      "health": "13087",
+      "mana": "2030",
+      "protection": "13087",
+      "strength": "15247"
+    },
+    "url": "/user/196472/"
+  },
+  "261073": {
+    "id": "261073",
+    "image": null,
+    "name": "Ryu Hayabusa",
+    "rank": "8",
+    "stats": {
+      "agility": "10682",
+      "health": "12679",
+      "mana": "2035",
+      "protection": "12679",
+      "strength": "14679"
+    },
+    "url": "/user/261073/"
+  },
+  "4168": {
+    "id": "4168",
+    "image": null,
+    "name": "Templarium",
+    "rank": "2",
+    "stats": {
+      "agility": "11541",
+      "health": "13701",
+      "mana": "2050",
+      "protection": "13701",
+      "strength": "15861"
+    },
+    "url": "/user/4168/"
+  },
+  "469274": {
+    "id": "469274",
+    "image": null,
+    "name": "Cachorro",
+    "rank": "10",
+    "stats": {
+      "agility": "10121",
+      "health": "12281",
+      "mana": "2045",
+      "protection": "12281",
+      "strength": "14441"
+    },
+    "url": "/user/469274/"
+  },
+  "5610": {
+    "id": "5610",
+    "image": null,
+    "name": "Fur Immer",
+    "rank": "9",
+    "stats": {
+      "agility": "10388",
+      "health": "12428",
+      "mana": "2050",
+      "protection": "12428",
+      "strength": "14468"
+    },
+    "url": "/user/5610/"
+  },
+  "5991": {
+    "id": "5991",
+    "image": null,
+    "name": "Yulina",
+    "rank": "1",
+    "stats": {
+      "agility": "11962",
+      "health": "14122",
+      "mana": "2040",
+      "protection": "14122",
+      "strength": "16282"
+    },
+    "url": "/user/5991/"
+  },
+  "626702": {
+    "id": "626702",
+    "image": null,
+    "name": "Capablank",
+    "rank": "6",
+    "stats": {
+      "agility": "10793",
+      "health": "12953",
+      "mana": "2000",
+      "protection": "12951",
+      "strength": "15113"
+    },
+    "url": "/user/626702/"
+  },
+  "728543": {
+    "id": "728543",
+    "image": null,
+    "name": "Astarotth",
+    "rank": "4",
+    "stats": {
+      "agility": "10967",
+      "health": "13127",
+      "mana": "1970",
+      "protection": "13127",
+      "strength": "15287"
+    },
+    "url": "/user/728543/"
+  },
+  "79158": {
+    "id": "79158",
+    "image": null,
+    "name": "Eternal Fantasy",
+    "rank": "3",
+    "stats": {
+      "agility": "11388",
+      "health": "13547",
+      "mana": "2010",
+      "protection": "13548",
+      "strength": "15709"
+    },
+    "url": "/user/79158/"
+  },
+  "819315": {
+    "id": "819315",
+    "image": null,
+    "name": "Rygar",
+    "rank": "7",
+    "stats": {
+      "agility": "10817",
+      "health": "12889",
+      "mana": "1935",
+      "protection": "12893",
+      "strength": "14977"
+    },
+    "url": "/user/819315/"
+  }
+}
---
+++ spider.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import json
+import mechanize
+import os
+import re
+
+
+class Stats(object):
+    """
+    This object was place inside of a Player
+    """
+
+    def __init__(self):
+        super(Stats, self).__init__()
+        # stats
+        self.strength = ""
+        self.health = ""
+        self.agility = ""
+        self.protection = ""
+        self.mana = ""
+
+
+class Image(object):
+    """docstring for Image"""
+
+    def __init__(self):
+        super(Image, self).__init__()
+        self.filename = ""
+
+    def write_to_file(self, browser, content_html):
+        image_tags = content_html.findAll('img')
+        for image in image_tags:
+            filename = image['src'].lstrip('http://')
+            filename = os.path.join(dir, filename.replace('/', '_'))
+            self.filename = filename
+            data = browser.open(image['src']).read()
+            browser.back()
+            save = open(filename, 'wb')
+            save.write(data)
+            save.close()
+
+    def remove(self):
+        os.remove(self.filename)
+
+
+class Player(object):
+    """docstring for Player"""
+
+    def __init__(self, stats=None, image=None):
+        super(Player, self).__init__()
+        self.id = ""
+        self.name = ""
+        self.rank = ""
+        self.url = ""
+        self.stats = stats
+        self.image = image
+
+    def set_id(self, id):
+        self.id = id
+
+    def set_rank(self, rank):
+        self.rank = rank
+
+    def set_url(self, url):
+        self.url = url
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_stats(self, stats_obj):
+        self.stats = stats_obj
+
+
+class ScrapperTitanWars(object):
+    """docstring for ScrapperTitanWars"""
+
+    def __init__(self):
+        super(ScrapperTitanWars, self).__init__()
+        self._url_root = "http://guerradetitanes.net"
+        self.__instance_browser(self)
+        self.invalid_players = {}
+        self.players = {}
+
+    @staticmethod
+    def __instance_browser(self):
+        browser = mechanize.Browser()
+        browser.set_handle_robots(False)
+        browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; \
+                                    en-US; rv:1.9.0.1) Gecko/2008071615 \
+                                    Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+        self.browser = browser
+
+    def sign_in(self, username, password):
+        url_login = self._url_root + "?sign_in=1"
+        self.browser.open(url_login)
+        self.browser.select_form(nr=0)
+        form = [f for f in self.browser.forms()][0]
+        form.new_control("text", "login", {"value": ""})
+        form.fixup()
+
+        # Logueo
+        self.browser.form["login"] = username
+        self.browser.form["pass"] = password
+        resp = self.browser.submit().read()
+
+        content_html = BeautifulSoup(resp, 'html.parser')
+
+        has_error = content_html.find('div', {'class': 'error'})
+        if has_error:
+            print has_error.text
+            exit(1)
+        else:
+            print 'Login complete'
+
+    def dump_all_data(self, f=None):
+        d = {}
+        for k in self.players:
+            d[k] = self.players[k].__dict__
+        for k in d:
+            d[k]['stats'] = d[k]['stats'].__dict__
+        json_data = json.dumps(
+            d,
+            sort_keys=True,
+            indent=2,
+            separators=(',', ': ')
+        )
+        if f:
+            f.write(json_data)
+            f.close()
+        else:
+            print json_data
+
+    def __clean_invalid_players(self):
+        for k in self.invalid_players:
+            self.players.pop(k)
+        self.invalid_players = {}
+
+    def hydrate_ranking(self, page):
+        """ Carga una colección de jugadores con el ranking, id y url
+        """
+        def __get_player_id(h):
+            return h.replace('/user/', '').replace('/', '') if h else None
+
+        url = self._url_root + "/rating/sumstat/{}".format(page)
+        self.browser.open(url)
+        resp = self.browser.response().read()
+        content_html = BeautifulSoup(resp, 'html.parser')
+        content_html = content_html.find_all('div', {'class': 'block_zero'})
+        for i, div in enumerate(content_html):
+            rank = div.find('span').get_text() if div.find('span') else None
+            name = div.find('a', href=True).string if div.find('a') else None
+            href = div.find('a', href=True)['href'] if div.find('a') else None
+
+            player_id = __get_player_id(href)
+
+            p = Player()
+            p.set_id(player_id)
+            p.set_rank(rank)
+            p.set_url(href)
+            p.set_name(name)
+            self.players.update({player_id: p})
+
+    def hydrate_player_stats(self):
+        """ Dada una colección de jugadores, instancia sus stats
+            Notar que si el jugador es inválido, lo descarta
+        """
+        def __get_value(exp):
+            try:
+                res = re.findall(r'\d+', exp).pop()
+            except:
+                res = None
+            return res
+
+        for k in self.players:
+            p = self.players[k]
+
+            try:
+                int(p.id)
+                self.browser.open(self._url_root + p.url)
+            except:
+                print 'This is an invalid player: {} so will be mark to invalid'.format(p.name)
+                self.invalid_players[k] = p
+                continue
+
+            resp = self.browser.response().read()
+            content_html = BeautifulSoup(resp, 'html.parser')
+            stats_container = content_html.find('div', {'class': 'block_zero'})
+            s = Stats()
+            s.strength = __get_value(stats_container.find(text=re.compile(u'Fuerza: ')))
+            s.agility = __get_value(stats_container.find(text=re.compile(u'Agilidad: ')))
+            s.health = __get_value(stats_container.find(text=re.compile(u'Salud: ')))
+            s.protection = __get_value(stats_container.find(text=re.compile(u'Protección: ')))
+            s.mana = __get_value(stats_container.find(text=re.compile(u'Energía: ')))
+
+            p.set_stats(s)
+
+        self.__clean_invalid_players()
+
+    def hydrate_player_image(self):
+        """
+        PRE: las urls deben ser válidas
+        """
+        for k in self.players:
+            p = self.players[k]
+            self.browser.open(self._url_root + p.url)
+            resp = self.browser.response().read()
+            content_html = BeautifulSoup(resp, 'html.parser')
+            first_link = content_html.find_all('div', {'class': 'float-left'})
+            import ipdb
+            ipdb.set_trace()
+            # TODO: seguir más tarde
+
+
+spider = ScrapperTitanWars()
+spider.sign_in("javierverb", "yourpasswordhere")
+spider.hydrate_ranking(1)
+spider.hydrate_player_image()
+# spider.hydrate_player_stats()
+# f = open('d', 'w+')
+# spider.dump_all_data(f)
